4062d267d8d70923774d4477368d40057bd66909
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-09-12 09:28:07 -0300

    cambio 2

---
+++ ejercicio 2.py
@@ -0,0 +1,4 @@
+#Leer dos números reales, calcular e imprimir los dos posibles cocientes entre ellos.
+numero1=int(input("ingrese un número: "))
+numero2=int(input("ingrese otro número: "))
+print(f"cosiente n°1: {numero1/numero2},cosiente n°2: {numero2/numero1}")
