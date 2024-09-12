27b6236906a8e490330af498a2db89899a74d2c5
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-09-12 08:33:48 -0300

    cambios1

---
+++ ejercicio 24.py
@@ -0,0 +1,9 @@
+#Hacer un programa que ingresando como datos:
+#   a. Kms. recorridos por un vehículo.
+#   b. Precio del combustible por litro.
+#   c. Kms. recorridos por cada litro
+# Calcule:
+#   i. La cantidad de litros consumidos
+#   ii. Importe gastado en combustible.
+#   iii. Imprimir los resultados
+#   iv. Ejemplificar y realizar la prueba de escritorio
