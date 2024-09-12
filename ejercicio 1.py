7b61da72d358c56d6c738c0458c0b3bc35244578
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-09-12 08:21:10 -0300

    inicio

---
+++ ejercicio 1.py
@@ -0,0 +1,3 @@
+#leer un número e imprimirlo junto con sus primeros múltiplos, ejemplifique para los primeros 4 múltiplos del número ingresado.
+numero=int(input("Ingrese un numero: "))
+print(f"{numero}{numero*2}{numero*3}{numero*4}{numero*5}")
